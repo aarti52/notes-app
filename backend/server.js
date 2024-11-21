c2eb4854a58a675ddc17e5000f703bf5bfb3143a
aarti <aartiaggarwal566@gmail.com>
2024-11-21 22:11:22 +0530

    'commit'

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import noteRoute from './Routes/noteRoute.js'
+import cors from 'cors'
+import process from 'process';
+import dotenv from 'dotenv'
+const app = express()
+
+dotenv.config()
+app.use(cors());
+
+
+app.use(express.json())
+const port = process.env.PORT ||4005
+const URI=process.env.MongoDbURI
+try {
+    mongoose.connect(URI)
+        console.log('connected to db');
+    } catch (error) {
+        console.log(error)
+    }
+app.get('/',(req,res)=>{
+    res.send('backend running successfully')
+})
+app.use('/api/notes',noteRoute)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
